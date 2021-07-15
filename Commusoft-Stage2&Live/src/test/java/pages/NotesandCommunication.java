7a99e9c9829afc2eecf514490723a5ede52c71c1
Bala <Bala@Balasubramanyan>
2021-07-15 13:22:26 +0530

    In smoke test fiel - commented some unwanted cases.
    notesandcommunication file - changed the emailaddress from ranjit's to
    ezhil's

--- Commusoft-Stage2&Live/src/test/java/pages/NotesandCommunication.java
+++ Commusoft-Stage2&Live/src/test/java/pages/NotesandCommunication.java
@@ -15,7 +15,7 @@ public class NotesandCommunication extends Wrapper {
 		Thread.sleep(3000);
 		click("//a[text()='Email invoice']");
 		Thread.sleep(2000);
-		selectdropdown("//select[@ng-model='email.selected_recipient_address']","ranjit@commusoft.com");
+		selectdropdown("//select[@ng-model='email.selected_recipient_address']","eezhilraja@commusoft.com");
 		Thread.sleep(1000);
 		click("(//a[@ng-click='sendEmail()'])[2]");
 		Thread.sleep(4000);

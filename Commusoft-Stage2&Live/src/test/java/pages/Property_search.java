2f23e54e28b93619bdade074fbeb49839cbd09a1
balasubramanyan <balasubramanyan@commusoft.com>
2021-11-15 12:00:14 +0530

    Customer and work address create scenarios. 24 cases added under lable
    folder

--- Commusoft-Stage2&Live/src/test/java/pages/Property_search.java
+++ Commusoft-Stage2&Live/src/test/java/pages/Property_search.java
@@ -4,18 +4,17 @@ import org.openqa.selenium.WebDriver;
 
 import action.Wrapper;
 
-public class Customer_search extends Wrapper {
+public class Property_search extends Wrapper {
 
-	public Customer_search(WebDriver ldriver)
+	public Property_search(WebDriver ldriver)
 	{
 		this.driver= ldriver;
 	}
 
-	public void global_search()
+	public void search_customer()
 	{
-
-		type("$q", "RanjitCustomer editCustomer edit1 Surnamecs Edit");
-
+		clear ("$q");
+		type("$q", "Testing Customer");
 		typeenter("$q");
 
 	}
@@ -23,11 +22,44 @@ public class Customer_search extends Wrapper {
 	public void click_customer()
 
 	{
-
-		click("//a [text() = 'Mr & Mrs RanjitCustomer editCustomer edit1 Surnamecs Edit']");
+       click("//a [text() = 'Mr & Mrs Testing Customer']");
+	}
+	public void search_workaddress()
+	{
+		clear ("$q");
+		type("$q", "Update name");
+		typeenter("$q");
 
 	}
 
+	public void click_workaddress()
+
+	{
+
+		click("//a [text() = 'Mrs Update name']");
+		
+
+	}
+	public void search_company()
+	{
+		clear ("$q");
+		type("$q", "AbC company");
+		typeenter("$q");
+	}
+	public void click_company()
+	{
+		click("//a [text() = 'AbC company']");
+	}
+	public void search_WA_as_company()
+	{
+		clear ("$q");
+		type("$q", "WA as Company");
+		typeenter("$q");
+	}
+	public void click_WA_as_company()
+	{
+		click("//a [text() = 'WA as Company']");
+	}
 
 
 }
